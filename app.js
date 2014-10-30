9ffde602834fd9c4e9bc10ec72a7fdc22fd58774
zestyphresh <zestyphresh@gmail.com>
2014-10-30 17:30:28 +0000

    sshhjk

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 (function () {
     
     $j = jQuery.noConflict();
-    var conn = new jsforce.Connection({ accessToken: '{!$API.Session_Id}' });
+    
     
     var dims = {}, groups = {};
     var data = crossfilter();
