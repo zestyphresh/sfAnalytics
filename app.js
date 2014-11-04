8498c31faacf839ee9dcd112831eb62409383ec5
zestyphresh <zestyphresh@gmail.com>
2014-11-04 10:40:52 +0000

    newl

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
     var dims = {}, groups = {};
     var data = crossfilter();
     
-    fetch().then(function(result) { console.log('result'); }).done()
+    fetch().then(function(result) { console.log(result); }).done()
   
     function fetch() {
         
@@ -15,14 +15,18 @@
         
         var records = [];
         
+        // conn.sobject("Account")
+        //     .select("Sub_Sector__c, Sales_Year_To_Date__c, Budget_Year_To_Date__c, Target_Year_To_Date__c, Sales_Previous_Year_To_Date__c")
+        //     .where("Sub_Sector__c IN ('Commercial', 'Hardware Wholesale')")
+        //     .execute({ autoFetch : true, maxFetch : 15000 }, function(err, records) {
+        //         if (err) { deferred.reject('error'); };
+                
+        //         console.log(records);
+        //     });
+            
         conn.sobject("Account")
             .select("Sub_Sector__c, Sales_Year_To_Date__c, Budget_Year_To_Date__c, Target_Year_To_Date__c, Sales_Previous_Year_To_Date__c")
-            .where("Sub_Sector__c = 'Commercial'")
-            .execute({ autoFetch : true, maxFetch : 15000 }, function(err, records) {
-                console.log(records);
-            });
-        
-        conn.query('SELECT Name, Parent_Name__c, Group_Name__c, Budget_Owner__c, Sales_Year_To_Date__c, Budget_Year_To_Date__c, Target_Year_To_Date__c FROM Account')
+            .where("Sub_Sector__c IN ('Commercial', 'Hardware Wholesale')")
             .on('record', function(record) {
                 records.push(record);
             })
@@ -33,6 +37,18 @@
                 deferred.resolve(records);
             })
             .run({ autoFetch : true, maxFetch : 15000 });
+        
+        // conn.query('SELECT Name, Parent_Name__c, Group_Name__c, Budget_Owner__c, Sales_Year_To_Date__c, Budget_Year_To_Date__c, Target_Year_To_Date__c FROM Account')
+        //     .on('record', function(record) {
+        //         records.push(record);
+        //     })
+        //     .on('error', function(query) {
+        //         deferred.reject('error');
+        //     })
+        //     .on('end', function(err) {
+        //         deferred.resolve(records);
+        //     })
+        //     .run({ autoFetch : true, maxFetch : 15000 });
 
         return deferred.promise;
 
