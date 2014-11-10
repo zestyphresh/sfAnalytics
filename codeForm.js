7153d140b6d940171cbfc493d26762fd692e15e6
zestyphresh <zestyphresh@gmail.com>
2014-11-10 21:38:05 +0000

    swgfhsgfhooooooooooooooooooooooooooooooooooooooooooooooo

--- codeForm.js
+++ codeForm.js
@@ -59,6 +59,17 @@
             "scrollX": true,
             'dom': 'T<"clear">lfrtip',
             'tableTools': {
+                "aButtons": [
+                    "copy",
+                    "csv",
+                    "xls",
+                    {
+                        "sExtends": "pdf",
+                        "sPdfOrientation": "landscape",
+                        "sPdfMessage": "Your custom message would go here."
+                    },
+                    "print"
+                ],
                 'sSwfPath': swf
             },
             'columns' : _columns,
