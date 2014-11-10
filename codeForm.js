7a56d8152953efe0732ed8d81e10b5e39ffbc3c0
zestyphresh <zestyphresh@gmail.com>
2014-11-10 21:06:29 +0000

    swgfhsgfhooooooooooooooooooooooooooooooooooooooooooooooo

--- codeForm.js
+++ codeForm.js
@@ -57,6 +57,9 @@
             'paging' : false,
             "scrollX": true,
             'columns' : _columns,
+            'columnDefs' : [
+                { "width": "9%", "targets": [0,1,2,3,4,5,6,7,8,9,10] }
+            ],
         });
     }
 
