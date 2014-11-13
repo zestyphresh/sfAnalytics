dc749f1b615437f548d19a42bc7302dfcdbc139e
zestyphresh <zestyphresh@gmail.com>
2014-11-13 12:37:33 +0000

    sikibibib

--- codeForm.js
+++ codeForm.js
@@ -19,8 +19,9 @@
         var records = [];
 
         conn.sobject("Product2")
-            .select("Part_Code__c, Name, Retail_Barcode__c, Retail_Barcode_Format__c, Retail_Barcode_Number__c, Inner_Transit_Barcode__c, Inner_Transit_Barcode_Format__c, Inner_Transit_Barcode_Number__c, " +
-                     "Outer_Transit_Barcode__c, Outer_Transit_Barcode_Format__c, Outer_Transit_Barcode_Number__c")
+            .select("Part_Code__c, Long_Name__c, Name, Retail_Barcode__c, Retail_Barcode_Format__c, Retail_Barcode_Number__c, Inner_Transit_Barcode__c, Inner_Transit_Barcode_Format__c, Inner_Transit_Barcode_Number__c, " +
+                    "Inner_Transit_Quantity__c, Outer_Transit_Barcode__c, Outer_Transit_Barcode_Format__c, Outer_Transit_Barcode_Number__c, Outer_Transit_Quantity__c, " +
+                    "Vendor__r.Vendor_Code_Name__c, Family, Mkt_Group__c, Retail_Packaging_Brand__c, Commodity_Code__c")
             .where("Project__c = " + "'" + projectId + "'")
             .on('record', function(record) {
                 records.push(record);
@@ -40,6 +41,7 @@
     function createTable(id, data) {
 
         var _columns = [{"data": "Part_Code__c", "title": "Code"},
+                        {"data": "Long_Name__c", "title": "Description"},
                         {"data": "Name", "title": "QAD Description"}, 
                         {"data": "Retail_Barcode__c", "title": "Retail Barcode Required"},                
                         {"data": "Retail_Barcode_Format__c", "title": "Retail Barcode Format"},                    
@@ -47,9 +49,16 @@
                         {"data": "Inner_Transit_Barcode__c", "title": "Inner Transit Barcode Required"},                      
                         {"data": "Inner_Transit_Barcode_Format__c", "title": "Inner Transit Barcode Format"},        
                         {"data": "Inner_Transit_Barcode_Number__c", "title": "Inner Transit Barcode Number"},
+                        {"data": "Inner_Transit_Quantity__c", "title": "Inner Transit Quantity"},
                         {"data": "Outer_Transit_Barcode__c", "title": "Outer Transit Barcode Required"},                      
                         {"data": "Outer_Transit_Barcode_Format__c", "title": "Outer Transit Barcode Format"},        
-                        {"data": "Outer_Transit_Barcode_Number__c", "title": "Outer Transit Barcode Number"}
+                        {"data": "Outer_Transit_Barcode_Number__c", "title": "Outer Transit Barcode Number"},
+                        {"data": "Outer_Transit_Quantity__c", "title": "Outer Transit Quantity"},
+                        {"data": "Vendor__r.Vendor_Code_Name__c", "title": "Vendor"},
+                        {"data": "Family", "title": "Design Group"},
+                        {"data": "Mkt_Group__c", "title": "Market Group"},
+                        {"data": "Retail_Packaging_Brand__c", "title": "Brand"},
+                        {"data": "Commodity_Code__c", "title": "Commodity Code"}
         ];
 
         var table = $j('#' + id).dataTable({
