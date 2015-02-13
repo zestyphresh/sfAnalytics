6f75f8263109aee00c84194986d49f615948fab1
zestyphresh <zestyphresh@gmail.com>
2015-02-13 13:58:13 +0000

    evwcfwegrhh5h5j5j

--- accountSales.js
+++ accountSales.js
@@ -1,61 +1,41 @@
 (function(salesforceConn, accId) {
     
     $j = jQuery.noConflict();
-    
-    console.log(sales);
-    
+
     //VARS
     var salesforce = {};
     var chart = {};
     var table = {};
     var value = {};
     
-    var sales = {
-        
-        query : {
-            sObject : 'Daily_Historical_Sales__c',
-            select : 'Invoice_Date__c, Fiscal_Year__c, Fiscal_Month__c, Value__c, Net_Value__c, Quantity__c, Is_Fiscal_Year_To_Date__c, Is_Fiscal_Last_Period__c, Product__r.Product_Code_Name__c, Product__r.Family, Promotion__r.Name',
-            where : 'Account__r.Id = ' + "'" + accId + "'",
-            maxfetch : 100000
-        },
-        
-        crossfilter : {
-            records : crossfilter(),
-            dims : {},
-            groups : {},
-            value : {},
-        }
-
+    var salesQuery = {
+        sObject : 'Daily_Historical_Sales__c',
+        select : 'Invoice_Date__c, Fiscal_Year__c, Fiscal_Month__c, Value__c, Net_Value__c, Quantity__c, Is_Fiscal_Year_To_Date__c, Is_Fiscal_Last_Period__c, Product__r.Product_Code_Name__c, Product__r.Family, Promotion__r.Name',
+        where : 'Account__r.Id = ' + "'" + accId + "'",
+        maxfetch : 100000
     }
-    
-    var forecast = {
         
-        query : {
-            sObject : 'Forecast2__c',
-            select : 'Fiscal_Year__c, Fiscal_Month__c, Value__c, Net_Value__c, Is_Fiscal_Year_To_Date__c, Is_Fiscal_Last_Period__c, Forecast_Brand__c, Forecast_Type__c"',
-            where : 'Account__r.Id = ' + "'" + accId + "'",
-            maxfetch : 100000
-        },
-        
-        crossfilter : {
-            records : crossfilter(),
-            dims : {},
-            groups : {},
-            value : {},
-        }
-
+    var forecastQuery = {
+        sObject : 'Forecast2__c',
+        select : 'Fiscal_Year__c, Fiscal_Month__c, Value__c, Net_Value__c, Is_Fiscal_Year_To_Date__c, Is_Fiscal_Last_Period__c, Forecast_Brand__c, Forecast_Type__c"',
+        where : 'Account__r.Id = ' + "'" + accId + "'",
+        maxfetch : 100000
     }
-    
-    Q.all(soql.call(sales), soql.call(forecast)).then(function(resSales, resForecast) {
         
-        init.apply(sales, resSales);
-        init.apply(forecast, resForecast);
+    Q.all( new soql(salesQuery), new soql(forecastQuery) ).then(function(resSales, resForecast) {
+        
+        console.log(resSales, resForecast);
+        
+        //init.apply(sales, resSales);
+        //init.apply(forecast, resForecast);
 
     }).done();
 
     //var utils = function() {
         
-        var soql = function() {
+        function soql(query) {
+            
+            this.query = query;
         
             var deferred = Q.defer();
                 
