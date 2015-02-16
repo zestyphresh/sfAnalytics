9f5fed2285c3461424dcaa6567b22d2b9bf15495
zestyphresh <zestyphresh@gmail.com>
2015-02-16 09:57:38 +0000

    evwcfwegrhh5h5j5j

--- accountSales.js
+++ accountSales.js
@@ -38,18 +38,18 @@
     function grossTable() {
         
         var source = [
-            {month : 1, sales : 0, budget : 0, target : 0, last : 0},
-            {month : 2, sales : 0, budget : 0, target : 0, last : 0},
-            {month : 3, sales : 0, budget : 0, target : 0, last : 0},
-            {month : 4, sales : 0, budget : 0, target : 0, last : 0},
-            {month : 5, sales : 0, budget : 0, target : 0, last : 0},
-            {month : 6, sales : 0, budget : 0, target : 0, last : 0},
-            {month : 7, sales : 0, budget : 0, target : 0, last : 0},
-            {month : 8, sales : 0, budget : 0, target : 0, last : 0},
-            {month : 9, sales : 0, budget : 0, target : 0, last : 0},
-            {month : 10, sales : 0, budget : 0, target : 0, last : 0},
-            {month : 11, sales : 0, budget : 0, target : 0, last : 0},
-            {month : 12, sales : 0, budget : 0, target : 0, last : 0}
+            {monthName : 'January', month : 1, sales : 0, budget : 0, vsBudget : 0, target : 0, vsTarget : 0, last : 0, vsLast : 0},
+            {monthName : 'February', month : 2, sales : 0, budget : 0, vsBudget : 0, target : 0, vsTarget : 0, last : 0, vsLast : 0},
+            {monthName : 'March', month : 3, sales : 0, budget : 0, vsBudget : 0, target : 0, vsTarget : 0, last : 0, vsLast : 0},
+            {monthName : 'April', month : 4, sales : 0, budget : 0, vsBudget : 0, target : 0, vsTarget : 0, last : 0, vsLast : 0},
+            {monthName : 'May', month : 5, sales : 0, budget : 0, vsBudget : 0, target : 0, vsTarget : 0, last : 0, vsLast : 0},
+            {monthName : 'June', month : 6, sales : 0, budget : 0, vsBudget : 0, target : 0, vsTarget : 0, last : 0, vsLast : 0},
+            {monthName : 'July', month : 7, sales : 0, budget : 0, vsBudget : 0, target : 0, vsTarget : 0, last : 0, vsLast : 0},
+            {monthName : 'August', month : 8, sales : 0, budget : 0, vsBudget : 0, target : 0, vsTarget : 0, last : 0, vsLast : 0},
+            {monthName : 'September', month : 9, sales : 0, budget : 0, vsBudget : 0, target : 0, vsTarget : 0, last : 0, vsLast : 0},
+            {monthName : 'October', month : 10, sales : 0, budget : 0, vsBudget : 0, target : 0, vsTarget : 0, last : 0, vsLast : 0},
+            {monthName : 'November', month : 11, sales : 0, budget : 0, vsBudget : 0, target : 0, vsTarget : 0, last : 0, vsLast : 0},
+            {monthName : 'December', month : 12, sales : 0, budget : 0, vsBudget : 0, target : 0, vsTarget : 0, last : 0, vsLast : 0}
         ];
         
         _.each(source, function(d) {
@@ -59,25 +59,31 @@
             d.budget = d3.sum(data.forecast, function(s) {
                 return s.Fiscal_Month__c == d.month && s.Fiscal_Year__c == 2015 && s.Forecast_Type__c == 'Budget' ? s.Gross_Value__c : 0; 
             });
+            d.vsBudget = d.sales - d.budget;
             d.target = d3.sum(data.forecast, function(s) {
                 return s.Fiscal_Month__c == d.month && s.Fiscal_Year__c == 2015 && (s.Forecast_Type__c == 'Budget' || s.Forecast_Type__c == 'Target')? s.Gross_Value__c : 0;
             });
+            d.vsTarget = d.sales - d.target;
             d.last = d3.sum(data.sales, function(s) {
                 return s.Fiscal_Month__c == d.month && s.Fiscal_Year__c == 2014 ? s.Value__c : 0;
             });
+            d.vsLast = d.sales - d.last;
         });
         
         console.log(source);
         
-        var tableCols = [{"data": "month", "title": "Month"},
+        var tableCols = [{"data": "monthName", "title": "Month"},
                          {"data": "sales", "title": "Gross Sales"},
                          {"data": "budget", "title": "Gross Budget"},
-                         {"data": "target", "title": "Gross Target"}, 
-                         {"data": "last", "title": "Last Year"}
+                         {"data": "vsBudget", "title": "Gross Budget"},
+                         {"data": "target", "title": "Gross Target"},
+                         {"data": "vsTarget", "title": "Gross Budget"},
+                         {"data": "last", "title": "Last Year"},
+                         {"data": "vsLast", "title": "Gross Budget"}
         ];
         
         var tableColDefs = [
-            {'targets' : [1,2,3,4], 
+            {'targets' : [1,2,3,4,5,6,7], 
             'render' : function (cell, type, row, meta) {
                 switch (type) {
                     case 'display':
