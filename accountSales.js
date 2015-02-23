5cb603fdabd58fc40092620a1dcd8cc508fc91f2
zestyphresh <zestyphresh@gmail.com>
2015-02-23 21:19:37 +0000

    evwcfwegrhh5h5j5j

--- accountSales.js
+++ accountSales.js
@@ -254,7 +254,8 @@
                     x: {
                         type: 'timeseries',
                         tick: {
-                            format : '%b',
+                            //format : '%b'
+                            format : function (e) { return moment(e).format('MMM').slice(0, 1); }
                         },
                         min : new Date(2010,0,1),
                         max : new Date(2015,11,1),
