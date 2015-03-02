57a1a405ce1e275f0aa79cdb30504320a5f891df
zestyphresh <zestyphresh@gmail.com>
2015-03-02 20:54:49 +0000

    newhvjukfdetgh

--- accountSalesBeta.js
+++ accountSalesBeta.js
@@ -69,6 +69,9 @@
         monthlySalesChart('#netByMonthChart', netSales);
         monthlySalesTable('#netByMonthTable', netSales);
         
+        //tabProductSales
+        productSales();
+        
 
         
     }).done();
