7e76448229bbae759f9f6e75156c6f67ab84a7f5
zestyphresh <zestyphresh@gmail.com>
2015-03-02 13:08:23 +0000

    newhvjuk

--- EndOfMonthData.js
+++ EndOfMonthData.js
@@ -18,7 +18,7 @@
         sObject : 'Daily_Historical_Sales__c',
         select : 'Account__r.Id, Fiscal_Year__c, Net_Value__c, Is_Fiscal_Year_To_Date__c, Is_Fiscal_Last_Period__c',
         where : 'Fiscal_Year__c >= 2014',
-        maxfetch : 200000
+        maxfetch : 300000
     };
         
     var forecastQuery = {
@@ -43,10 +43,12 @@
         var fiscal = resDate.value[0];
         
         _.each(result, function(acc) {
+            
+            console.log(acc);
 
-            acc.sales = d3.sum(sales, function(s) { return s.Account__r.Id == acc.Id && s.Fiscal_Year__c == 2015 && s.Is_Fiscal_Year_To_Date__c == true ? s.Net_Value__c : 0; })
+            acc.sales = d3.sum(sales, function(s) { console.log(s); return s.Account__r.Id == acc.Id && s.Fiscal_Year__c == 2015 && s.Is_Fiscal_Year_To_Date__c == true ? s.Net_Value__c : 0; })
             acc.last = d3.sum(sales, function(s) { return s.Account__r.Id == acc.Id && s.Fiscal_Year__c == 2014 && s.Is_Fiscal_Year_To_Date__c == true ? s.Net_Value__c : 0; })
-            acc.budget = d3.sum(forecast, function(s) { return s.Account__r.Id == acc.Id && s.Fiscal_Year__c == 2015 && s.Is_Fiscal_Year_To_Date__c == true && s.Forecast_Type__c == 'Budget' ? s.Net_Value__c : 0; })
+            acc.budget = d3.sum(forecast, function(s) { console.log(s); return s.Account__r.Id == acc.Id && s.Fiscal_Year__c == 2015 && s.Is_Fiscal_Year_To_Date__c == true && s.Forecast_Type__c == 'Budget' ? s.Net_Value__c : 0; })
             acc.target = d3.sum(forecast, function(s) { return s.Account__r.Id == acc.Id && s.Fiscal_Year__c == 2015 && s.Is_Fiscal_Year_To_Date__c == true && (s.Forecast_Type__c == 'Budget' || s.Forecast_Type__c == 'Target') ? s.Net_Value__c : 0; })
             
             acc.salesL = d3.sum(sales, function(s) { return s.Account__r.Id == acc.Id && s.Fiscal_Year__c == 2015 && s.Is_Fiscal_Last_Period__c == true ? s.Net_Value__c : 0; })
